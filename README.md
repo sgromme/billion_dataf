179c360249787b80ff7714ce65e14f32c9524f20
sgromme <wills1377@gmail.com>
2026-03-03 12:11:10 -0500

    updates

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+/home/sgromme/source/1brc/data/measurements.txt
+
+
+
+# time to run  
+
+real    0m51.831s
+user    3m5.765s
+sys     1m10.641s
+
+
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::functions_aggregate::expr_fn::{avg, max, min};
+use datafusion::prelude::*;
+use tokio::runtime::Runtime;
+
+fn main() {
+    // put in the path
+    let path = "/home/sgromme/source/1brc/data/measurements.txt";
+    let _path = "/home/sgromme/source/billion_dataf/measurements.txt";
+
+    let rt = Runtime::new().unwrap();
+    let ctx = SessionContext::new();
+
+    let station_field = Field::new("station", DataType::Utf8, false);
+    let temp_field = Field::new("temperature", DataType::Float32, false);
+
+    let schema = Schema::new(vec![station_field, temp_field]);
+
+    let opts = CsvReadOptions::new()
+        .delimiter(b';')
+        .has_header(false)
+        .file_extension("txt")
+        .schema(&schema);
+
+    let df = rt.block_on(ctx.read_csv(_path, opts)).unwrap();
+
+    let results_fut = df
+        .aggregate(
+            vec![col("station")],
+            vec![
+                min(col("temperature")).alias("min_temp"),
+                avg(col("temperature")).alias("mean_temp"),
+                max(col("temperature")).alias("max_temp"),
+            ],
+        )
+        .unwrap()
+        .sort(vec![col("station").sort(true, false)])
+        .unwrap()
+        .collect();
+
+    let results = rt.block_on(results_fut);
+
+    let pretty = datafusion::arrow::util::pretty::pretty_format_batches(&results.unwrap()).unwrap();
+
+    println!("{pretty}");
+}
